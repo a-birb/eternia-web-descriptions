f87dad846695f3afc08e5cc2b60a6d230797e060
Birb <rainydayinside@gmail.com>
2023-09-22 19:18:55 -0400

    update

--- randomimg.js
+++ randomimg.js
@@ -1,9 +1,8 @@
-window.onload = choosePic;
 const fs = require('fs');
 
 var myPix = fs.readdirSync("images/");
 
 function choosePic() {
      var randomNum = Math.floor(Math.random() * myPix.length);
-     document.getElementById("myPicture").src = myPix[randomNum];
+     return document.getElementById("myPicture").src = myPix[randomNum];
 }
