e8b034801d8830ccfddaa1afd8b6dbf31f712638
Birb <rainydayinside@gmail.com>
2023-09-22 19:27:34 -0400

    update 3

--- randomimg.js
+++ randomimg.js
@@ -1,8 +1,31 @@
-const fs = require('fs');
+var fs = require('fs');
 
-var myPix = fs.readdirSync("images/");
+function readFiles(dirname, onFileContent, onError) {
+  fs.readdir(dirname, function(err, filenames) {
+    if (err) {
+      onError(err);
+      return;
+    }
+    filenames.forEach(function(filename) {
+      fs.readFile(dirname + filename, 'utf-8', function(err, content) {
+        if (err) {
+          onError(err);
+          return;
+        }
+        onFileContent(filename, content);
+      });
+    });
+  });
+}
+
+var data = {};
+readFiles('dirname/', function(filename, content) {
+  data[filename] = content;
+}, function(err) {
+  throw err;
+});
 
 function choosePic() {
-     var randomNum = Math.floor(Math.random() * myPix.length);
-     return document.getElementById("myPicture").src = myPix[randomNum];
+     var randomNum = Math.floor(Math.random() * data.length);
+     return document.getElementById("myPicture").src = data[randomNum];
 }
