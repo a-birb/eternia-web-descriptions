da2b7c24dcc2447938b60434df2754e2debd9d2e
Birb <rainydayinside@gmail.com>
2023-09-22 21:22:55 -0400

    images add

--- randomimg.js
+++ randomimg.js
@@ -1,6 +1,6 @@
 window.onload = choosePic;
 
-var data = new Array("images/wolf-decap.jpg","images/wolf-demons.jpg","images/wolf-grin.jpg","images/wolf-manyheads.jpg","images/wolf-smile.jpg","images/wolf-splayed.jpg");
+var data = new Array("images/wolf-decap.jpg","images/wolf-demons.jpg","images/wolf-grin.jpg","images/wolf-manyheads.jpg","images/wolf-smile.jpg","images/wolf-splayed.jpg","images/wolf-sheep.jpg","images/wolf-overbearing.jpg");
 
 function choosePic() {
      var randomNum = Math.floor(Math.random() * data.length);
