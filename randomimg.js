d95c7b6d4d2892bcbb11c8210986b7ad6989052f
Birb <rainydayinside@gmail.com>
2024-03-19 07:47:18 -0400

    new desc

--- randomimg.js
+++
@@ -1,8 +0,0 @@
-window.onload = choosePic;
-
-var data = new Array("images/wolf-decap.jpg","images/wolf-demons.jpg","images/wolf-grin.jpg","images/wolf-manyheads.jpg","images/wolf-smile.jpg","images/wolf-splayed.jpg","images/wolf-sheep.jpg","images/wolf-overbearing.jpg");
-
-function choosePic() {
-     var randomNum = Math.floor(Math.random() * data.length);
-     document.getElementById("playby").src = data[randomNum];
-}
